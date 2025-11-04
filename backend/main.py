d85879f4bd44488aecf85bf091950ca8fbf01a92
Alba Segura <m1ss.sunrse@gmail.com>
2025-11-04 15:27:47 +0100

    feat: Add WebSocket endpoint and HTML client for real-time CPU monitoring

---
+++ backend/main.py
@@ -0,0 +1,46 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+import psutil
+import asyncio
+import json
+import docker
+
+app = FastAPI(title="Performance Dashboard API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Performance Dashboard API Running"}
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()  # Accept the connection
+    
+    try:
+        while True:
+            # Send simple message every 2 seconds
+            message = {
+                "cpu": psutil.cpu_percent(interval=1),
+                "cpucore": psutil.cpu_percent(interval=1, percpu=True),
+                "cores": psutil.cpu_count(logical=False),
+                "threads": psutil.cpu_count(),
+                "frequency":psutil.cpu_freq(),
+                "frequencycore":psutil.cpu_freq(percpu=True),
+                "timestamp": asyncio.get_event_loop().time()
+            }
+            await websocket.send_text(json.dumps(message))
+            await asyncio.sleep(2)
+            
+    except Exception as e:
+        print(f"Connection closed: {e}")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
