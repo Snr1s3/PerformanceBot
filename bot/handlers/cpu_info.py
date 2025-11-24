87804bc546c7bf4e3866ae6560576b037db2129d
Alba Segura <m1ss.sunrse@gmail.com>
2025-11-24 09:46:58 +0100

    fix: Correct method calls in CpuHandler and DiskHandler to use appropriate functions

--- bot/handlers/cpu_info.py
+++ bot/handlers/cpu_info.py
@@ -5,6 +5,6 @@ from bot.handlers.base_info import BaseInfo
 
 class CpuHandler(BaseInfo):
     async def cpu_Handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        content = self.system()
+        content = self.cpu()
         msg = "<b>CPU INFO:</b>\n" + "\n".join(content)
         await update.message.reply_text(msg, parse_mode="HTML")
--- bot/handlers/disk_info.py
+++ bot/handlers/disk_info.py
@@ -4,6 +4,6 @@ from bot.handlers.base_info import BaseInfo
 
 class DiskHandler(BaseInfo):
     async def disk_Handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-        content = self.cpu()
+        content = self.disk()
         msg = "<b>DISK INFO:</b>\n" + "\n".join(content)
         await update.message.reply_text(msg, parse_mode="HTML")
