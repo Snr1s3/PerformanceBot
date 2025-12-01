13883450177558c141077d94d4bfea0778991b1f
Alba Segura <m1ss.sunrse@gmail.com>
2025-12-01 12:16:39 +0100

    feat: Add CPU info logging and create Notify Me workflow for GitHub Actions

--- bot/handlers/cpu_info.py
+++ bot/handlers/cpu_info.py
@@ -6,5 +6,13 @@ from .base_info import BaseInfo
 class CpuHandler(BaseInfo):
     async def cpu_Handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         content = self.cpu()
+        chat = update.effective_chat
+        info = (
+            f"Chat ID: {chat.id}\n"
+            f"Type: {chat.type}\n"
+            f"Title: {chat.title}\n"
+            f"Username: {chat.username}\n"
+        )
+        print(info)
         msg = "<b>CPU INFO:</b>\n" + "\n".join(content)
         await self.send_long_message(msg, update, parse_mode="HTML")
