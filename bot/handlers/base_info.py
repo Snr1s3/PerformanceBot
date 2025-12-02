8d5569c459b3068e345194d0407b3784fbd98322
Alba Segura <m1ss.sunrse@gmail.com>
2025-12-02 09:39:10 +0100

    style: Improve readability by formatting multi-line return statements in BaseInfo methods

--- bot/handlers/base_info.py
+++ bot/handlers/base_info.py
@@ -19,30 +19,55 @@ class BaseInfo:
 
     async def send_long_message(self, message, update, **kwargs):
         for i in range(0, len(message), self.MAX_LEN):
-            await update.message.reply_text(message[i:i+self.MAX_LEN], **kwargs)
+            await update.message.reply_text(
+                message[i:i+self.MAX_LEN], **kwargs
+            )
 
     def system(self):
-        return "<b>SYSTEM INFO:</b>\n" + "\n".join(SystemInfo(self.sock).fetch())
+        return (
+            "<b>SYSTEM INFO:</b>\n"
+            + "\n".join(SystemInfo(self.sock).fetch())
+        )
 
     def cpu(self):
-        return "<b>CPU INFO:</b>\n" + "\n".join(CpuInfo(self.sock).fetch())
+        return (
+            "<b>CPU INFO:</b>\n"
+            + "\n".join(CpuInfo(self.sock).fetch())
+        )
 
     def docker(self):
-        safe_content = [html.escape(line)
-                        for line in DockerInfo(self.sock).fetch()]
-        return "<b>DOCKER INFO:</b>\n" + "\n".join(safe_content)
+        safe_content = [
+            html.escape(line)
+            for line in DockerInfo(self.sock).fetch()
+        ]
+        return (
+            "<b>DOCKER INFO:</b>\n"
+            + "\n".join(safe_content)
+        )
 
     def memory(self):
-        return "<b>MEMORY INFO:</b>\n" + "\n".join(MemoryInfo(self.sock).fetch())
+        return (
+            "<b>MEMORY INFO:</b>\n"
+            + "\n".join(MemoryInfo(self.sock).fetch())
+        )
 
     def sensors(self):
-        return "<b>Sensors INFO:</b>\n" + "\n".join(SensorsInfo(self.sock).fetch())
+        return (
+            "<b>Sensors INFO:</b>\n"
+            + "\n".join(SensorsInfo(self.sock).fetch())
+        )
 
     def disk(self):
-        return "<b>DISK INFO:</b>\n" + "\n".join(DiskInfo(self.sock).fetch())
+        return (
+            "<b>DISK INFO:</b>\n"
+            + "\n".join(DiskInfo(self.sock).fetch())
+        )
 
     def network(self):
-        return "<b>NETWORK INFO:</b>\n" + "\n".join(NetworkInfo(self.sock).fetch())
+        return (
+            "<b>NETWORK INFO:</b>\n"
+            + "\n".join(NetworkInfo(self.sock).fetch())
+        )
 
     def capture(self):
         return CaptureInfo(self.sock).fetch()
